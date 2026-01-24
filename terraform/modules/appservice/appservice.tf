8ee726bdfb9768e029db3f7ba2c3bc4d94be2a4f
Tunde Oretade <tundeiness@yahoo.co.uk>
2026-01-24 10:26:44 -0800

    edit appservice.tf

--- terraform/modules/appservice/appservice.tf
+++ terraform/modules/appservice/appservice.tf
@@ -4,15 +4,15 @@ resource "random_id" "unique" {
 
 resource "azurerm_service_plan" "test" {
   name                = "${var.application_type}-${var.resource_type}-plan-${random_id.unique.hex}"
-  location            = var.location  # Use same location variable
+  location            = "East US 2"  # Use same location variable
   resource_group_name = var.resource_group
-  os_type             = "Linux"
+  os_type             = "Windows" # "Linux"
   sku_name            = "F1"
 }
 
 resource "azurerm_linux_web_app" "test" {
   name                = "${var.application_type}-${var.resource_type}-app-${random_id.unique.hex}"
-  location            = var.location  # Use same location as Service Plan
+  location            = "East US 2"  # same location as Service Plan
   resource_group_name = var.resource_group
   service_plan_id     = azurerm_service_plan.test.id
 
@@ -21,6 +21,6 @@ resource "azurerm_linux_web_app" "test" {
   }
   
   site_config {
-    always_on = false  # This is correct for Y1/Consumption plan
+    always_on = false  # for Y1/Consumption plan
   }
 }
