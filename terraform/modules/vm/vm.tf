015d16249e8f0c6927180b4457ec7fd70ee0ebdc
Tunde Oretade <tundeiness@yahoo.co.uk>
2026-02-01 14:23:25 -0800

    clean repo of details for fresh start

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -20,8 +20,8 @@ resource "azurerm_linux_virtual_machine" "vm" {
   network_interface_ids = [azurerm_network_interface.vm_nic.id]
   admin_ssh_key {
     username   = var.admin_username
-    public_key = var.ssh_public_key
-    # public_key = file("/Users/tunde/Desktop/udrsa/.ssh/id_rsa.pub")
+    # public_key = var.ssh_public_key
+    public_key = file("/Users/tunde/Desktop/udrsa/.ssh/id_rsa.pub")
   }
   os_disk {
     caching           = "ReadWrite"
